9f60346813f56223baa3c4fb2013f5c508a3306c
Ramakant Debata <ramakant.s.debata@gmail.com>
2024-01-24 09:47:56 +0530

    IsIterable

---
+++ 21B_CheckIterable.py
@@ -0,0 +1,15 @@
+def isIterable(obj):
+    pass
+
+if __name__ == "__main__":
+    a = 10
+    print(isIteratable(a))
+    b = "Hello"
+    print(isIterable(b))
+    c = [10, 2]
+    print(isIterable(c))
+    d = (10, 2)
+    print(isIterable(d))
+    e = {10, 2}
+    print(isIterable(e))
+    
